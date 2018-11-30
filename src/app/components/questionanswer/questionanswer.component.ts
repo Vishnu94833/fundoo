72ae68923c8e179caa546345d68e050ef5361a33
Vishnu94833 <vishnu94833@gmail.com>
2018-11-30 20:01:55 +0530

    commit

--- src/app/components/questionanswer/questionanswer.component.ts
+++ src/app/components/questionanswer/questionanswer.component.ts
@@ -17,6 +17,7 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
   message: any;
   title: any;
   description: any;
+  private userArray: any=[];
   constructor(private notesService: NotesService, private route: Router,
     private questionService: QuestionanswerService) { }
   private url = this.route.url;
@@ -29,24 +30,33 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
   private noteDetails = this.url.split('/');
   private id = localStorage.getItem('userId')
   private image = localStorage.getItem('imageUrl');
-  private img = environment.apiUrl + this.image;
+  private userImage = environment.apiUrl ;
   private email = localStorage.getItem('email');
   private replyMessage: any = [];
   private userName: any = [];
-  private userImage: any = [];
   private questionName: any = [];
   private liked = 0;
-
+  private replied = 0;
+  private parentId;
+    private askQuestion;
   ngOnInit() {
     this.getDetails();
-    this.questionAnswerReply(this.reply);
+    // this.questionAnswerReply(this.reply,);
     console.log(this.id)
     console.log(this.url)
   }
 
-  likedNote() {
-    this.liked = 1;
-    let parentId = this.questionArray['questionAndAnswerNotes'][0].id
+repliedFunc(){
+  this.replied = 1;
+}
+
+repliedNotFunc(){
+  this.replied = 0;
+}
+
+  likedNote(parentId) {
+    this.liked = 0;
+    // let parentId = this.questionArray['questionAndAnswerNotes'][0].id
     this.questionService.likeToQuestion(parentId,
       {
         "like": true
@@ -55,29 +65,9 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$))
       .subscribe(
         (data) => {
+          this.liked=1;
           let count = data['data']['details'].count;
           console.log('liked is successful', data);
-        },
-        error => {
-          console.log("Error", error);
-        });
-
-  }
-
-  unLikedNote() {
-    this.liked = 0;
-    let parentId = this.questionArray['questionAndAnswerNotes'][0].id
-    this.questionService.likeToQuestion(parentId,
-      {
-        "like": false
-      }
-    )
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (data) => {
-          let count = data['data']['details'].count;
-          count = 0;
-          console.log('unliked is successful', data['data']['details'].count);
         },
         error => {
           console.log("Error", error);
@@ -93,26 +83,19 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$)).subscribe(
         (data) => {
           console.log('data', data);
-          // for(var i =0; i<=data['data'][0]['questionAndAnswerNotes'].length;i++){
           this.questionArray = data['data']['data'][0]
           this.questionName = data['data']['data'][0]['questionAndAnswerNotes']
           console.log(this.questionName)
           this.title = this.questionArray.title;
           this.description = data['data']['data'][0].description
-          // this.questionCheckList = data['data']['data'][0].noteCheckLists
           this.messageArray = this.questionArray['questionAndAnswerNotes']
-          // for(var i=0;i<=this.questionName.length;i++){
-            this.userName=this.messageArray[0]['user'].email
-         
-          console.log(this.userName)
-          // }
-          
-          // this.userName = this.messageArray['user'].username
-          this.userImage = this.messageArray[0]['user'].imageUrl
+          // this.userArray = this.questionArray['questionAndAnswerNotes'][0]
+            this.userName=this.messageArray[0]
           console.log(this.userName)
           console.log(this.messageArray);
           if (this.questionArray['questionAndAnswerNotes'][0] != undefined) {
             this.message = this.questionArray['questionAndAnswerNotes'][0].message;
+            this.parentId = this.questionArray['questionAndAnswerNotes'][0].id;
           }
         },
         error => { 
@@ -135,6 +118,7 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
       .subscribe(
         (data) => {
           this.questionAnswer = data['data']['details']
+
           console.log('data', data);
         },
         error => {
@@ -153,9 +137,7 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$))
       .subscribe(
         (data) => {
-          // for(let i=1;i<=data['data']['data']['questionAndAnswerNotes'].length;i++){
           this.replyMessage = data['data']['data']['questionAndAnswerNotes']
-          // }
         console.log('data', data);
         },
         error => {
@@ -164,11 +146,11 @@ export class QuestionanswerComponent implements OnInit, OnDestroy {
   }
 
 
-  questionAnswerRate() {
-    let parentId = this.questionArray['questionAndAnswerNotes'][0].id
+  questionAnswerRate(parentId,event) {
+    // let parentId = this.questionArray['questionAndAnswerNotes'][0].id
     this.questionService.rateToQuestion(parentId,
       {
-        "rate": 4
+        "rate": event
       }
     )
       .pipe(takeUntil(this.destroy$))
